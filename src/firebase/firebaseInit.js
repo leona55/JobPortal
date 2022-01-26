802b640fc039a49b5879a5beb9aa78f38cd656c7
leona55 <mavrajn@gmail.com>
2022-01-26 20:13:18 +0100

    Commit

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA3ryT1fZwbaSNumHH20NvAa3zPkY63F7c",
+  authDomain: "smoothieapp-dc49e.firebaseapp.com",
+  projectId: "smoothieapp-dc49e",
+  storageBucket: "smoothieapp-dc49e.appspot.com",
+  messagingSenderId: "184792580386",
+  appId: "1:184792580386:web:a78e2416b4c1291afda57b"
+};
