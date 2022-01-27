a6940c9929a89bf8179a5b9e8b42497b4312b08a
leona55 <mavrajn@gmail.com>
2022-01-27 20:00:27 +0100

    Creating SmoothiePost

---
+++ src/components/SmoothiePost.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+      <h2>Hello</h2>
+  </div>
+</template>
+
+<script>
+export default {
+name: "smoothiePost",
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,37 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <SmoothiePost :post="welcomeScreen" />
+    <SmoothiePost :post="post" v-for="(post, index) in sampleSmoothiePost" :key="index" />
+  </div>
 </template>
 
 <script>
+import SmoothiePost from "../components/SmoothiePost.vue";
 export default {
   name: "Home",
-  components: {},
+  components: {SmoothiePost},
+  data() {
+    return {
+      welcomeScreen: {
+        title: "Welcome!",
+        smoothiePost:
+        "Weekly smoothie recipes. Register today to never miss a post with new recipe",
+        welcomeScreen: true,
+        photo: "1",
+      },
+      sampleSmoothiePost: [
+        {
+          title: "This is a Filler Title!",
+          smoothieHTML: "This is a filler smoothie post title!",
+          smoothieCoverPhoto: "2",
+        },
+        {
+          title: "This is a Filler Title!",
+          smoothieHTML: "This is a filler smoothie post title!",
+          smoothieCoverPhoto: "3",
+        },
+      ],
+    };
+  },
 };
 </script>
