657b7d88cf60adfc55ec27944f6631936701b239
leona55 <mavrajn@gmail.com>
2022-01-27 15:18:13 +0100

    Navbar

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -1,4 +1,5 @@
-import firebase from 'firebase/app'
+import firebase from 'firebase/app';
+import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA3ryT1fZwbaSNumHH20NvAa3zPkY63F7c",
@@ -8,3 +9,9 @@ const firebaseConfig = {
   messagingSenderId: "184792580386",
   appId: "1:184792580386:web:a78e2416b4c1291afda57b"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export {timestamp};
+export default firebaseApp.firestore();
