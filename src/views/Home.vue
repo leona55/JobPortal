3813fdca8c17bf54839973a23cd2b497dcf799b4
leona55 <mavrajn@gmail.com>
2022-01-28 13:09:53 +0100

    Update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,7 +13,7 @@
     <div class="updates">
       <div class="container">
         <h2>Never miss a post. Register for your free account today!</h2>
-        <router-link class="router button" to="#">
+        <router-link class="router-button" to="#">
           Register for Smoothies <Arrow class="arrow arrow-light"/>
         </router-link>
       </div>
@@ -79,6 +79,28 @@ export default {
     @media(min-width: 800px){
       padding: 125px 25px;
       flex-direction: row;
+    }
+
+    .router-button{
+      display: flex;
+      font-size: 14px;
+      text-decoration: none;
+      @media(min-width: 800px){
+          margin-left: auto;
+      }
+    }
+
+    h2{
+      font-weight: 300;
+      font-size: 32px;
+      max-width: 425px;
+      width: 100%;
+      text-align: center;
+      text-transform: uppercase;
+      @media(min-width: 800px){
+        text-align: initial;
+        font-size: 40px;
+      }
     }
   }
 }
