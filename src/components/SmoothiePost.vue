f7d70673ad302ed46df7752299c70b40ad54f5b1
Fortesa <fr47258@ubt-uni.net>
2022-02-15 12:14:15 +0100

    Commit

--- src/components/SmoothiePost.vue
+++ src/components/SmoothiePost.vue
@@ -42,6 +42,7 @@ export default {
 };
 </script>
 
+
 <style lang="scss" scoped>
 .smoothie-wrapper {
   display: flex;
--- src/router/index.js
+++ src/router/index.js
@@ -11,18 +11,29 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      title: 'Home'
+    }
   },
   {
     path: "/smoothies",
     name: "Smoothies",
     component: Smoothies,
+    meta: {
+      title: 'Smoothies'
+    }
   },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes,
+  routes, 
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} |  Smoothie`;
+  next();
 });
 
 export default router;
