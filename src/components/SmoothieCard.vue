637b9a1f85a479a9ed1af36e0fdc53afd21df7b8
Albiona19 <ab47853@ubt-uni.net>
2022-02-18 17:31:48 +0100

    Test

--- src/components/SmoothieCard.vue
+++ src/components/SmoothieCard.vue
@@ -51,7 +51,7 @@ export default {
   background-color: #fff;
   min-height: 420px;
   transition: 0.5s ease all;
-
+//commit
   &:hover {
     transform: rotateZ(-1deg) scale(1.01);
     box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
