dc07e603e0acd59d1c8514dd3634a38f09f42085
leona55 <mavrajn@gmail.com>
2022-02-18 15:35:22 +0100

    Some errors resolved

--- src/components/SmoothieCard.vue
+++ src/components/SmoothieCard.vue
@@ -9,12 +9,7 @@
       </div>
     </div>
     <img
-<<<<<<< HEAD
-      :src="require(`../assets/SmoothieCards/${post.smoothieCoverPhoto}.jpg`)"
-=======
-      :src="require(`../assets/smoothieCards/${post.smoothieCoverPhoto}.jpg`)"
->>>>>>> 047ca5620f496c77eb6f21d0b1b62de2e32911fd
-      alt=""
+      :src="require(`../assets/smoothieCards/${post.smoothieCoverPhoto}.jpg`)" alt=""
     />
     <div class="info">
       <h4>{{ post.smoothieTitle }}</h4>
