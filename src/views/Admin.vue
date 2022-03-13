68193851ebd23817fe4fcb0c4b9e238201640cd6
Albiona19 <84641601+Albiona19@users.noreply.github.com>
2022-03-14 00:09:38 +0200

    Style some colors changes in Admin

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -58,7 +58,7 @@ export default {
       border-radius: 8px;
       box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
       padding: 32px;
-      background-color: #f1f1f1;
+      background-color: pink;
       display: flex;
       flex-direction: column;
       max-width: 600px;
