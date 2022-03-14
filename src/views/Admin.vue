4ff739015c27256c04fa648c7fd7a5b3ddc6d67d
Albiona Berisha <ab47853@ubt-uni.net>
2022-03-14 02:28:46 +0200

    New change

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -56,7 +56,7 @@ export default {
 
     .admin-info {
       border-radius: 90px;
-      box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+      box-shadow: 0 3px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
       padding: 40px;
       background-color: pink;
       display: flex;
